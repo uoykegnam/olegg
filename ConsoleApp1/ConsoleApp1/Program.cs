4029a97e501a0f7ae19d69bc499f86650982ee5a
username <dsgfghhfhj@gmail.com>
2020-03-12 13:44:19 +0300

    prompt user for last name as well.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,10 +6,12 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string name;
-            Console.WriteLine("Enter your name: ");
-            name = Console.ReadLine();
-            Console.WriteLine("Hello{0}", name);
+            string last, first;
+            Console.WriteLine("Enter your first name: ");
+            first = Console.ReadLine();
+            Console.WriteLine("Enter your last name: ");
+            last = Console.ReadLine();
+            Console.WriteLine("Hello {0} {1}", first, last);
             Console.ReadLine();
         }
     }
